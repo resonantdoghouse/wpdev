1dead2e6f1a2160770d72342e783f83a26571ca7
Jim Bennett <buildcreativewebsites@gmail.com>
2017-06-09 14:11:32 -0700

    showing saved options

--- themes/wpdev/includes/admin/options-page.php
+++ themes/wpdev/includes/admin/options-page.php
@@ -1,6 +1,7 @@
 <?php
 
 function wpdev_theme_opts_page(){
+  $theme_opts               = get_option( 'wpdev_opts' );
 ?>
 
 <div class="wrap">
@@ -9,6 +10,17 @@ function wpdev_theme_opts_page(){
             <h4 class="panel-title"><?php _e('WpDev Theme Settings', 'wpdev' ); ?></h4>
         </div>
         <div class="panel-body row">
+
+            <?php
+            if(isset($_GET['status']) && $_GET['status'] == 1){
+              ?>
+              <div class="col s12">
+                <h6 class="blue-text">Success!!!</h6>
+              </div>
+
+              <?php
+            }
+            ?>
 
             <form class="col s12" method="post" action="admin-post.php">
 
@@ -19,19 +31,22 @@ function wpdev_theme_opts_page(){
                     <h5><?php _e('Social Icons', 'wpdev'); ?></h5>
                     <div class="col s4">
                         <div class="input-field">
-                            <input type="text" class="form-control" name="wpdev_inputTwitter" id="wpdev_inputTwitter">
+                            <input type="text" class="form-control" name="wpdev_inputTwitter" id="wpdev_inputTwitter"
+                                   value="<?php echo $theme_opts['twitter']?>">
                             <label for="wpdev_inputTwitter"><?php _e('Twitter', 'wpdev'); ?></label>
                         </div>
                     </div>
                     <div class="col s4">
                         <div class="input-field">
-                            <input type="text" class="form-control" name="wpdev_inputFacebook" id="wpdev_inputFacebook">
+                            <input type="text" class="form-control" name="wpdev_inputFacebook" id="wpdev_inputFacebook"
+                                   value="<?php echo $theme_opts['facebook']?>">
                             <label for="wpdev_inputFacebook"><?php _e('Facebook', 'wpdev'); ?></label>
                         </div>
                     </div>
                     <div class="col s4">
                         <div class="input-field">
-                            <input type="text" class="validate" name="wpdev_inputYoutube" id="wpdev_inputYoutube">
+                            <input type="text" class="validate" name="wpdev_inputYoutube" id="wpdev_inputYoutube"
+                                   value="<?php echo $theme_opts['youtube']?>">
                             <label for="wpdev_inputYoutube"><?php _e('YouTube', 'wpdev'); ?></label>
                         </div>
                     </div>
@@ -43,7 +58,7 @@ function wpdev_theme_opts_page(){
                         <div class="input-field">
                             <select name="wpdev_inputLogoType" id="wpdev_inputLogoType">
                                 <option value="1"><?php _e('Site Name', 'wpdev'); ?></option>
-                                <option value="2"><?php _e('Image', 'wpdev'); ?></option>
+                                <option value="2" <?php echo $theme_opts['logo_type'] == 2 ? 'SELECTED' : '' ;?>><?php _e('Image', 'wpdev'); ?></option>
                             </select>
                             <label for="wpdev_inputLogoType"><?php _e('Logo Type', 'wpdev'); ?></label>
                         </div>
@@ -67,7 +82,7 @@ function wpdev_theme_opts_page(){
                     <div class="col s12">
                         <div class="input-field">
                             <label><?php _e('Footer Text (HTML Allowed)', 'wpdev'); ?></label>
-                            <textarea class="materialize-textarea" name="wpdev_inputFooter"></textarea>
+                            <textarea class="materialize-textarea" name="wpdev_inputFooter"><?php echo stripslashes_deep($theme_opts['footer'])?></textarea>
                         </div>
                         <div class="input-field">
                             <button type="submit" class="btn btn-primary"><?php _e('Update', 'wpdev'); ?></button>
