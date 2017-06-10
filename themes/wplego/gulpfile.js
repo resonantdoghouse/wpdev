691f577d2f346c65c8d934a7e018015d74e5894f
Jim Bennett <buildcreativewebsites@gmail.com>
2017-06-09 18:09:29 -0700

    added gulp and basic tasks

---
+++ themes/wplego/gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    postcss = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer'),
+    cssvars = require('postcss-simple-vars'),
+    nested = require('postcss-nested'),
+    cssImport = require('postcss-import');
+
+gulp.task('default', function() {
+    console.log('default task has run');
+});
+
+gulp.task('php', function() {
+    console.log('php task has run');
+});
+
+gulp.task('styles', function() {
+    return gulp.src('./assets/css/styles.css')
+        .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+        .pipe(gulp.dest('./assets/build/styles'));
+});
+
+gulp.task('watch', function() {
+
+    watch('./**/*.php', function() {
+        gulp.start('html');
+    });
+
+    watch('./assets/css/**/*.css', function() {
+        gulp.start('styles');
+    });
+
+});
