8334f0e70cd9c6212d1712d0b87777a9836ef1a9
Jim Bennett <buildcreativewebsites@gmail.com>
2017-06-11 14:11:44 -0700

    updated saving recipe post and getting values

--- plugins/recipe/inc/admin/recipe-options.php
+++ plugins/recipe/inc/admin/recipe-options.php
@@ -1,5 +1,47 @@
 <?php
 
-function r_recipe_options_mb(){
-    echo 'hello';
+function r_recipe_options_mb( $post ){
+
+    $recipe_data              = get_post_meta( $post->ID, 'recipe_data', true );
+
+    if(!$recipe_data){
+        $recipe_data          =  array(
+            'ingredients'     => '',
+            'time'            => '',
+            'utensils'        => '',
+            'level'           => 'Beginner',
+            'meal_type'       => ''
+        );
+    }
+
+    ?>
+    <div class="form-group">
+        <label>Ingredients</label>
+        <input type="text" class="form-control" name="r_inputIngredients"
+               value="<?php echo $recipe_data['ingredients']; ?>">
+    </div>
+    <div class="form-group">
+        <label>Cooking Time</label>
+        <input type="text" class="form-control" name="r_inputTime"
+               value="<?php echo $recipe_data['time']; ?>">
+    </div>
+    <div class="form-group">
+        <label>Cooking Utensils</label>
+        <input type="text" class="form-control" name="r_inputUtensils"
+               value="<?php echo $recipe_data['utensils']; ?>">
+    </div>
+    <div class="form-group">
+        <label>Cooking Level</label>
+        <select class="form-control" name="r_inputLevel">
+            <option value="Beginner">Beginner</option>
+            <option value="Intermediate" <?php echo $recipe_data['level'] == "Intermediate" ? 'SELECTED' : ''; ?>>Intermediate</option>
+            <option value="Expert" echo $recipe_data['level'] == "Expert" ? 'SELECTED' : '';>Expert</option>
+        </select>
+    </div>
+    <div class="form-group">
+        <label>Meal Type</label>
+        <input type="text" class="form-control" name="r_inputMealType"
+               value="<?php echo $recipe_data['meal_type']; ?>">
+    </div>
+    <?php
 }
--- plugins/recipe/index.php
+++ plugins/recipe/index.php
@@ -21,11 +21,14 @@ define( 'RECIPE_PLUGIN_URL', __FILE__ );
 include( 'inc/activate.php' );
 include( 'inc/init.php' );
 include( 'inc/admin/init.php' );
+include( 'process/save-post.php' );
 
 
 // Hooks
 register_activation_hook( __FILE__, 'r_activate_plugin' );
 add_action( 'init', 'recipe_init' );
 add_action( 'admin_init', 'recipe_admin_init' );
+add_action( 'save_post_recipe', 'r_save_post_admin', 10, 3 );
+
 
 // Shortcodes
