524b64454b96e1675cc82405c9c2d9b4f1e0652d
Jim Bennett <buildcreativewebsites@gmail.com>
2017-06-11 10:35:09 -0700

    added post type for recipe and custom metabox starting point

---
+++ plugins/recipe/inc/admin/recipe-options.php
@@ -0,0 +1,5 @@
+<?php
+
+function r_recipe_options_mb(){
+    echo 'hello';
+}
---
+++ plugins/recipe/index.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Plugin Name: Recipe
+ * Description: Simple recipe plugin with ratings
+ * Version: 0.0.1
+ * Author URI: http://wpfork.com
+ * Text Domain: recipe
+ */
+
+
+if( !function_exists( 'add_action' ) ){
+    echo 'Not allowed!';
+    exit();
+}
+
+
+// Setup
+
+
+// Includes
+include( 'inc/activate.php' );
+include( 'inc/init.php' );
+include( 'inc/admin/init.php' );
+
+
+// Hooks
+register_activation_hook( __FILE__, 'r_activate_plugin' );
+add_action( 'init', 'recipe_init' );
+add_action( 'admin_init', 'recipe_admin_init' );
+
+// Shortcodes
