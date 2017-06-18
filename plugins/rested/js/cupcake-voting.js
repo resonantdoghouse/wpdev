18a2c0237816996c0babb20569a0c4d9cd7180a6
Jim Bennett <buildcreativewebsites@gmail.com>
2017-06-18 14:19:03 -0700

    added ajax voting and auth

--- plugins/rested/js/cupcake-voting.js
+++ plugins/rested/js/cupcake-voting.js
@@ -1,14 +1,31 @@
 (function($){
     $(document).ready(function(){
-        $.ajax({
-            url: cupcake_voting_data.base_url + 'votes',
-            method: 'POST',
-            // beforeSend: function (xhr) {
-            //     xhr.setRequestHeader( 'X-WP-Nonce', cupcake_voting_data.nonce );
-            // },
-            data: {
-                id: 521
-            }
+
+        $('.cupcake_vote').one('click', function(event){
+            event.preventDefault();
+
+            var postId = $(this).attr('data-post-id');
+            var el = this;
+
+            $.ajax({
+                url: cupcake_voting_data.base_url + 'votes',
+                method: 'POST',
+                beforeSend: function (xhr) {
+                    xhr.setRequestHeader( 'X-WP-Nonce', cupcake_voting_data.nonce );
+                },
+                data: {
+                    id: postId
+                }
+            }).done(function(data){
+                $(el).replaceWith(cupcake_voting_data.success_message);
+                $('#vote-count-' + postId).text(data);
+            }).fail(function(jqXHR, textStatus, errorThrown){
+                if ('undefined'!= typeof jqXHR.responseJSON.message ) {
+                    $(el).replaceWith(jqXHR.responseJSON.message);
+                    return;
+                }
+            });
         });
+
     });
 })(jQuery);
--- plugins/rested/rested.php
+++ plugins/rested/rested.php
@@ -10,7 +10,7 @@
  * License: GPLv2 or later
  */
 
-define( 'CUPCAKE_VOTING_VERSION', 1 );
+define( 'CUPCAKE_VOTING_VERSION', 1.1 );
 
 
 function cupcake_activate(){
@@ -32,26 +32,16 @@ function cupcake_register_endpoints(){
 					'validate_callback' => function( $param, $request, $key ){
 						return is_numeric( $param ) and ! is_null( get_post( $param ) );
 					},
-					'sanitize_calback' => 'absint'
+					'sanitize_callback' => 'absint'
 				)
 			),
 			'permission_callback' => function(){
-				return current_user_can( 'manage_options' );
+				return is_user_logged_in();
 			}
 		)
 	);
 }
 add_action( 'rest_api_init', 'cupcake_register_endpoints' );
-
-
-function cupcake_add_vote( WP_REST_Request $request ){
-	$votes = intval( get_post_meta( $request->get_param( 'id' ), 'votes', true ) );
-	if ( false === (bool) update_post_meta( $request->get_param( 'id' ), 'votes', $votes +1 ) )
-		{
-			return new WP_ERROR( 'vote_error', __( 'Unable to add vote', 'rested' ), $request->get_param( 'id' ) );
-		}
-		return $votes + 1;
-}
 
 
 function cupcake_enqueue_scripts(){
@@ -66,9 +56,50 @@ function cupcake_enqueue_scripts(){
 		'cupcake-voting-js',
 		'cupcake_voting_data',
 		array(
-			'nonce'     => wp_create_nonce( 'wp_rest' ),
-			'base_url'  => rest_url( '/cupcake-voting/v1/' )
+			'nonce'             => wp_create_nonce( 'wp_rest' ),
+			'base_url'          => rest_url( '/cupcake-voting/v1/' ),
+			'success_message'   => __( 'Thanks for voting!', 'rested' ),
+			'error_message'     => __( 'Error processing vote', 'rested' )
 		)
 	);
 }
 add_action( 'wp_enqueue_scripts', 'cupcake_enqueue_scripts' );
+
+
+function cupcake_add_vote( WP_REST_Request $request ){
+	$votes = intval( get_post_meta( $request->get_param( 'id' ), 'votes', true ) );
+	if ( false === (bool) update_post_meta( $request->get_param( 'id' ), 'votes', $votes +1 ) )
+		{
+			return new WP_ERROR( 'vote_error', __( 'Unable to add vote', 'rested' ), $request->get_param( 'id' ) );
+		}
+		return $votes + 1;
+}
+
+
+function cupcake_add_vote_html( $content ){
+	if ( 'post'== get_post_type() and is_main_query() ) {
+		$content .= '<p>' . sprintf(
+				esc_html( __( '%s%d%s vote(s)', 'rested' ) ),
+				'<span class="vote-count" id="vote-count-' . get_the_ID() . '">',
+					intval( get_post_meta( get_the_ID(), 'votes', true ) ),
+				'</span>'
+			) . '</p>';
+		$content .= '<p><a href="#" class="cupcake_vote" data-post-id="' . get_the_ID() . '">' .
+	                esc_html( __( 'Vote!', 'rested' ) ) .
+	                '</a></p>';
+	}
+	return $content;
+}
+add_filter( 'the_excerpt', 'cupcake_add_vote_html' );
+
+
+
+
+
+
+
+
+
+
+
+
