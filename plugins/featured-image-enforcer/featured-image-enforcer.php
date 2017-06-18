a26c68ee61944b93d56f8a696dc841a8479ef819
Jim Bennett <buildcreativewebsites@gmail.com>
2017-06-17 20:49:33 -0700

    Updated alert message to use wp_localize_script

--- plugins/featured-image-enforcer/featured-image-enforcer.php
+++ plugins/featured-image-enforcer/featured-image-enforcer.php
@@ -1,27 +1,27 @@
 <?php
-/*
-Plugin Name: Featured Image Enforcer
-Plugin URI: https://buildcreativewebsites.com
-Description: Forces featured image set before publishing post
-Version: 1.0
-Author: BCW
-Author URI: https://buildcreativewebsites.com
-License: GPLv2 or later
-*/
 
 define( 'FEATURED_ENFORCER_VERSION', 1 );
 
-function featured_image_enforcer_enqueue_scripts( $hook ){
-  if( 'post-new.php' != $hook )
-    return;
+function featured_image_enforcer_enqueue_scripts( $hook ) {
+	if ( 'post-new.php' != $hook ) {
+		return;
+	}
 
-  wp_enqueue_script(
-    'featured-image-enforcer',
-    plugins_url( 'js/featured-image-enforcer.js', __FILE__ ),
-    array( 'jquery' ),
-    FEATURED_ENFORCER_VERSION,
-    true
-  );
+	wp_enqueue_script(
+		'featured-image-enforcer',
+		plugins_url( 'js/featured-image-enforcer.js', __FILE__ ),
+		array( 'jquery' ),
+		FEATURED_ENFORCER_VERSION,
+		true
+	);
+
+	wp_localize_script(
+		'featured-image-enforcer',
+		'featured_image_data',
+		array(
+			'message' => __( 'Please select a featured image first', 'featured-image-enforcer' )
+		)
+	);
 }
 
 add_action( 'admin_enqueue_scripts', 'featured_image_enforcer_enqueue_scripts' );
--- plugins/featured-image-enforcer/js/featured-image-enforcer.js
+++ plugins/featured-image-enforcer/js/featured-image-enforcer.js
@@ -1,10 +1,10 @@
-jQuery(document).ready(function($){
+jQuery(document).ready(function ($) {
 
-  $('#publish').on('click', function(event){
-    if( $('#set-post-thumbnail img').length == 0 ){
-      event.preventDefault();
-      alert('Please select a featured image');
-    }
-  });
-  
+    $('#publish').on('click', function (event) {
+        if ($('#set-post-thumbnail img').length == 0) {
+            event.preventDefault();
+            alert(featured_image_data.message);
+        }
+    });
+
 });
